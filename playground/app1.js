1a7e4b634d0d2a1ff1c063f0a77a67f3f63574c1
Prajakta <prajaktakamble42@gmail.com>
2023-07-04 20:49:54 +0530

    add and remove the item

--- playground/app1.js
+++ playground/app1.js
@@ -1,25 +1,101 @@
-const template = (
-  <div>
-    <div className="title">
-      <h2>ToDo App</h2>
-      <p>what do you want to do today?</p>
-    </div>
+let items = [];
 
-    <div className="add-todo">
-      <div className="input-group mb-3">
-        <textarea
-          className="form-control"
-          placeholder="enter your todo here"
-          name="item"
-          rows="3"
-        ></textarea>
+const markItemComplete = (itemToRemove) => {
+  //   //remove the item from items array
 
-        <button className="btn btn-success" type="button" id="button-addon2">
-          Add
-        </button>
+  //method1
+  //   for (let index = 0; index < items.length; index++) {
+  //     const item = items[index];
+  //     if (item.id == itemToRemove.id) {
+  //       //found the item to be deleted
+  //       items.splice(index, 1);
+  //       break;
+  //     }
+  //   }
+
+  //method 2
+  items = items.filter((item) => item.id != itemToRemove.id);
+
+  render();
+};
+
+const addItem = (e) => {
+  const title = e.target.elements.itemtext.value;
+  items.push({
+    id: items.length + 1,
+    status: "open",
+    title,
+  });
+
+  //clear the input
+  e.target.elements.itemtext.value = "";
+  e.preventDefault();
+
+  render();
+};
+
+const render = () => {
+  const template = (
+    <div>
+      <div className="title">
+        <h2>ToDo App</h2>
+        <p>what do you want to do today?</p>
+      </div>
+      <div className="add-todo">
+        <form onSubmit={addItem}>
+          <div className="input-group mb-3">
+            <textarea
+              className="form-control"
+              placeholder="enter your todo here"
+              name="itemtext"
+              rows="3"
+            ></textarea>
+
+            <button
+              className="btn btn-success"
+              type="submit"
+              id="button-addon2"
+            >
+              Add
+            </button>
+          </div>
+        </form>
+      </div>
+
+      {items.length > 0 && (
+        <div className="items-list">
+          {items.map((item) => {
+            return (
+              <div className="input-group mb-3">
+                <input
+                  readOnly
+                  value={item.title}
+                  className="form-control"
+                  type="text"
+                  rows="3"
+                />
+
+                <button
+                  onClick={() => {
+                    markItemComplete(item);
+                  }}
+                  className="btn btn-danger"
+                  type="button"
+                  id="button-addon2"
+                >
+                  Complete
+                </button>
+              </div>
+            );
+          })}
+        </div>
+      )}
+      <div className="footer">
+        <p>copyrighted to praj</p>
       </div>
     </div>
-  </div>
-);
+  );
 
-ReactDOM.render(template, document.getElementById("app"));
+  ReactDOM.render(template, document.getElementById("app"));
+};
+render();
--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,128 @@
 "use strict";
 
-var template = React.createElement(
-  "div",
-  null,
-  React.createElement(
+var items = [];
+
+var markItemComplete = function markItemComplete(itemToRemove) {
+  //   //remove the item from items array
+
+  //method1
+  //   for (let index = 0; index < items.length; index++) {
+  //     const item = items[index];
+  //     if (item.id == itemToRemove.id) {
+  //       //found the item to be deleted
+  //       items.splice(index, 1);
+  //       break;
+  //     }
+  //   }
+
+  //method 2
+  items = items.filter(function (item) {
+    return item.id != itemToRemove.id;
+  });
+
+  render();
+};
+
+var addItem = function addItem(e) {
+  var title = e.target.elements.itemtext.value;
+  items.push({
+    id: items.length + 1,
+    status: "open",
+    title: title
+  });
+
+  //clear the input
+  e.target.elements.itemtext.value = "";
+  e.preventDefault();
+
+  render();
+};
+
+var render = function render() {
+  var template = React.createElement(
     "div",
-    { className: "title" },
-    React.createElement(
-      "h2",
-      null,
-      "ToDo App"
-    ),
-    React.createElement(
-      "p",
-      null,
-      "what do you want to do today?"
-    )
-  ),
-  React.createElement(
-    "div",
-    { className: "add-todo" },
+    null,
     React.createElement(
       "div",
-      { className: "input-group mb-3" },
-      React.createElement("textarea", {
-        className: "form-control",
-        placeholder: "enter your todo here",
-        name: "item",
-        rows: "3"
-      }),
+      { className: "title" },
       React.createElement(
-        "button",
-        { className: "btn btn-success", type: "button", id: "button-addon2" },
-        "Add"
+        "h2",
+        null,
+        "ToDo App"
+      ),
+      React.createElement(
+        "p",
+        null,
+        "what do you want to do today?"
+      )
+    ),
+    React.createElement(
+      "div",
+      { className: "add-todo" },
+      React.createElement(
+        "form",
+        { onSubmit: addItem },
+        React.createElement(
+          "div",
+          { className: "input-group mb-3" },
+          React.createElement("textarea", {
+            className: "form-control",
+            placeholder: "enter your todo here",
+            name: "itemtext",
+            rows: "3"
+          }),
+          React.createElement(
+            "button",
+            {
+              className: "btn btn-success",
+              type: "submit",
+              id: "button-addon2"
+            },
+            "Add"
+          )
+        )
+      )
+    ),
+    items.length > 0 && React.createElement(
+      "div",
+      { className: "items-list" },
+      items.map(function (item) {
+        return React.createElement(
+          "div",
+          { className: "input-group mb-3" },
+          React.createElement("input", {
+            readOnly: true,
+            value: item.title,
+            className: "form-control",
+            type: "text",
+            rows: "3"
+          }),
+          React.createElement(
+            "button",
+            {
+              onClick: function onClick() {
+                markItemComplete(item);
+              },
+              className: "btn btn-danger",
+              type: "button",
+              id: "button-addon2"
+            },
+            "Complete"
+          )
+        );
+      })
+    ),
+    React.createElement(
+      "div",
+      { className: "footer" },
+      React.createElement(
+        "p",
+        null,
+        "copyrighted to praj"
       )
     )
-  )
-);
+  );
 
-ReactDOM.render(template, document.getElementById("app"));
+  ReactDOM.render(template, document.getElementById("app"));
+};
+render();
